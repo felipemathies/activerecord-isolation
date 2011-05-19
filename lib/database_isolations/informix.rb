3f3f915618fc73bb1e6e20b58ebe8bd823755309
Felipe Mathies <felipe.mathies@gmail.com>
2011-05-19 15:07:08 -0300

    check load error

--- lib/database_isolations/informix.rb
+++ lib/database_isolations/informix.rb
@@ -1,13 +1,16 @@
-require 'active_record/connection_adapters/informix_adapter'
-
-ActiveRecord::ConnectionAdapters::InformixAdapter.class_eval do
-  def dirty
-    execute('SET ISOLATION TO DIRTY READ')
+begin
+  require 'active_record/connection_adapters/informix_adapter'
+  
+  ActiveRecord::ConnectionAdapters::InformixAdapter.class_eval do
+    def dirty
+      execute('SET ISOLATION TO DIRTY READ')
+    end
+    def clean
+      execute('SET ISOLATION TO REPEATABLE READ')
+    end
+    def reallyclean
+      execute('SET ISOLATION TO READ COMMITTED')
+    end
   end
-  def clean
-    execute('SET ISOLATION TO REPEATABLE READ')
-  end
-  def reallyclean
-    execute('SET ISOLATION TO READ COMMITTED')
-  end
+rescue LoadError
 end
--- lib/database_isolations/sqlite.rb
+++ lib/database_isolations/sqlite.rb
@@ -1,10 +1,13 @@
-require 'active_record/connection_adapters/sqlite_adapter'
+begin  
+  require 'active_record/connection_adapters/sqlite_adapter'
 
-ActiveRecord::ConnectionAdapters::SQLiteAdapter.class_eval do
-  def dirty
+  ActiveRecord::ConnectionAdapters::SQLiteAdapter.class_eval do
+    def dirty
+    end
+    def clean
+    end
+    def reallyclean
+    end
   end
-  def clean
-  end
-  def reallyclean
-  end
+rescue LoadError
 end
